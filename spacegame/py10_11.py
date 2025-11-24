029fd3c05127824b429e3a873c9634dcb3a98cb5
Bethany Wickson-Dix <22513062@bucks.ac.uk>
2025-11-24 11:34:17 +0000

    merge code

--- spacegame/py10_11.py
+++ spacegame/py10_11.py
@@ -10,6 +10,8 @@ import time
 
 pygame.init()
 
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 500
 
 
 #SET SCREEN SIZE
@@ -33,6 +35,11 @@ bulletx2 = 0
 alienplayercollision = False
 lives = 3
 
+#Invader rows
+invader_startrow = 100
+invader_endrow = 300
+invader_startcol = 100
+invader_endcol = 400 
 
 #LOAD AND SCALE BULLET SPRITE
 bullet_img = pygame.image.load("spacegame\_bullet.png")
@@ -59,6 +66,48 @@ zerolives = pygame.image.load("spacegame\_0lives.png")
 zerolives = pygame.transform.scale(zerolives, (110, 30)) 
 
 
+invader_img = pygame.image.load("spacegame\_invader1.png")
+invader_img = pygame.transform.scale(invader_img, (30, 30))
+
+
+
+#boolean for direction
+move_right = True
+edge_hit = False
+
+
+def draw_invaders():
+    for row in range(invader_startrow, invader_endrow, 30): # intervals of 30 
+        for col in range(invader_startcol, invader_endcol, 30):
+            screen.blit(invader_img, (col, row))
+
+def move_invaders():
+    global invader_startcol, invader_endcol, invader_startrow, invader_endrow, move_right
+    # start moving right 
+    if move_right == True:
+        invader_startcol += 1
+        invader_endcol += 1
+        edge_hit = False
+    else: # otherwise move left
+        invader_startcol -= 1
+        invader_endcol -= 1
+        edge_hit = False
+    
+    # detect edge of screen
+    if invader_endcol > SCREEN_WIDTH or invader_startcol < 0:
+        edge_hit = True
+        invader_startrow += 20
+        invader_endrow += 20
+    
+    # immediately reset edge_hit to prevent getting stuck! 
+    if edge_hit == True:
+        edge_hit == False
+        if move_right == True:
+            move_right = False
+        else:
+            move_right = True
+
+
 #START MAIN GAME LOOP, EACH RUN OF THIS LOOP IS A FRAME OF THE GAME
 running = True
 while running:
@@ -66,6 +115,10 @@ while running:
     #FILL THE WINDOW WITH A BLACK BACKGROUND
     #THIS IS DONE AT THE START OF THE GAME LOOP TO ENSURE EVERYTHING WE WANT TO SHOW ON THE SCREEN IS DRAWN ON TOP OF THE BACKGROUND
     screen.fill([0,0,0])
+
+    move_invaders()
+    
+    draw_invaders()
 
     #LIFE CHECKER
     #THIS PART CHECKS THE AMOUNT OF LIVES THE PLAYER HAS EVERY FRAME AND UPDATES THE LIVES ICONS TO MATCH THEIR AMOUNT OF LIVES
